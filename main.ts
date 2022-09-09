2c5148cf33aa11f8fd830c086cfd518f5e40b08f
santos <96233951+santosvdw@users.noreply.github.com>
2022-09-09 12:42:27 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
-})
-radio.sendNumber(0)
-basic.forever(function () {
-	
-})
+
